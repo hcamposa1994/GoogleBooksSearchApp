f3c6c759151b502ea22b50b2cbbbe2b1c731523e
Herbert Campos <wohoforever@yahoo.com>
2019-08-29 14:13:11 -0700

    edited book model to include an image and link proporty

--- models/book.js
+++ models/book.js
@@ -3,9 +3,10 @@ const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
   title: { type: String, required: true },
-  author: { type: String, required: true },
-  synopsis: String,
-  date: { type: Date, default: Date.now }
+  authors: { type: String, required: true },
+  synopsis: { type: String },
+  image: { type: String },
+  link: { type: String }
 });
 
 const Book = mongoose.model("Book", bookSchema);
