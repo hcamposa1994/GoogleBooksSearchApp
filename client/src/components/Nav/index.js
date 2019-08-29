41078dc84fec209d0bbec0913e1f053fbbd81000
Herbert Campos <wohoforever@yahoo.com>
2019-08-29 14:30:35 -0700

    edited the navbar with two buttons to be able to go to both saved and searched books

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -6,6 +6,16 @@ function Nav() {
       <a className="navbar-brand" href="/">
         React Reading List
       </a>
+      <div className="collapse navbar-collapse">
+        <ul className="navbar-nav">
+          <li className="nav-item active">
+            <a className="nav-link" href="/search">Search</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/saved">Saved</a>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
